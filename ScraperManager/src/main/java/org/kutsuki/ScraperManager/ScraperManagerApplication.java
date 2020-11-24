d97f23600c1177626455442bcf350c51bf5e65d7
MatchaGreen <MatchaGreen@192.168.86.88>
2020-11-23 21:21:57 -0500

    first

---
+++ ScraperManager/src/main/java/org/kutsuki/ScraperManager/ScraperManagerApplication.java
@@ -0,0 +1,13 @@
+package org.kutsuki.ScraperManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraperManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraperManagerApplication.class, args);
+	}
+
+}
